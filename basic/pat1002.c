fda9a2187c2bd4f98b63931465ef5b783bc1fbed
xiaorong61 <xiaorong61@gmail.com>
2019-02-11 21:52:17 +0800

    edit basic 2

--- basic/pat1002.c
+++ basic/pat1002.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+
 typedef char *String;
 String pinyinMap[] = {"ling", "yi",  "er", "san", "si",
                       "wu",   "liu", "qi", "ba",  "jiu"};
+
 void printNumByPinyin(int num) {
   if (num >= 10) {
     printNumByPinyin(num / 10);
@@ -10,10 +12,14 @@ void printNumByPinyin(int num) {
   }
   printf("%s", pinyinMap[num % 10]);
 }
-char *tail(char str[]) { return str + 1; }
-int sum(char str[]) {
-  return strlen(str) == 0 ? 0 : str[0] - '0' + sum(tail(str));
+
+int _sum(String str, int start, int end) {
+  if (start > end) return 0;
+  return str[start] - '0' + _sum(str, start + 1, end);
 }
+
+int sum(String str) { return _sum(str, 0, strlen(str) - 1); }
+
 int main() {
   char str[101];
   scanf("%s", str);
