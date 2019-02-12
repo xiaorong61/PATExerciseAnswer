82a71268d6f909eb2f74122b8d40797e021f94e3
xiaorong61 <xiaorong61@gmail.com>
2019-02-12 16:11:02 +0800

    结构体转换成数组

--- basic/pat1015.c
+++ basic/pat1015.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct Student {
-  int ID;
-  int de;
-  int cai;
-} Student;
-int compare(const void *_left, const void *_right) {
-  Student *left = (Student *)_left;
-  Student *right = (Student *)_right;
-  if (left->de + left->cai > right->de + right->cai) return -1;
-  if (left->de + left->cai == right->de + right->cai && left->de > right->de)
-    return -1;
-  if ((left->de + left->cai == right->de + right->cai &&
-       left->de == right->de && left->ID < right->ID))
+#include <string.h>
+enum index { ID, DE, CAI };
+int compare(const void *_l, const void *_r) {
+  int *l = (int *)_l;
+  int *r = (int *)_r;
+  if (l[DE] + l[CAI] > r[DE] + r[CAI]) return -1;
+  if (l[DE] + l[CAI] == r[DE] + r[CAI] && l[DE] > r[DE]) return -1;
+  if ((l[DE] + l[CAI] == r[DE] + r[CAI] && l[DE] == r[DE] && l[ID] < r[ID]))
     return -1;
   return 1;
 }
 int main() {
   int N, L, H, count[4] = {0};
   scanf("%d %d %d", &N, &L, &H);
-  Student result[4][N];
+  int result[4][N][CAI + 1];
   for (int i = 0; i < N; i++) {
-    Student stu;
-    scanf("%d %d %d", &(stu.ID), &(stu.de), &(stu.cai));
-    if (stu.de < L || stu.cai < L) continue;
-    if (stu.de >= H && stu.cai >= H)
-      result[0][count[0]++] = stu;
-    else if (stu.de >= H && stu.cai < H)
-      result[1][count[1]++] = stu;
-    else if (stu.de < H && stu.cai < H && stu.de >= stu.cai)
-      result[2][count[2]++] = stu;
+    int stu[CAI + 1];
+    scanf("%d %d %d", &(stu[ID]), &(stu[DE]), &(stu[CAI]));
+    if (stu[DE] < L || stu[CAI] < L) continue;
+    if (stu[DE] >= H && stu[CAI] >= H)
+      memcpy(result[0][count[0]++], stu, sizeof(stu));
+    else if (stu[DE] >= H && stu[CAI] < H)
+      memcpy(result[1][count[1]++], stu, sizeof(stu));
+    else if (stu[DE] < H && stu[CAI] < H && stu[DE] >= stu[CAI])
+      memcpy(result[2][count[2]++], stu, sizeof(stu));
     else
-      result[3][count[3]++] = stu;
+      memcpy(result[3][count[3]++], stu, sizeof(stu));
   }
   printf("%d\n", count[0] + count[1] + count[2] + count[3]);
   for (int i = 0; i < 4; i++) {
-    qsort(result[i], count[i], sizeof result[i][0], compare);
+    qsort(result[i], count[i], sizeof result[0][0], compare);
     for (int j = 0; j < count[i]; j++)
-      printf("%d %d %d\n", result[i][j].ID, result[i][j].de, result[i][j].cai);
+      printf("%d %d %d\n", result[i][j][ID], result[i][j][DE],
+             result[i][j][CAI]);
   }
   return 0;
 }
