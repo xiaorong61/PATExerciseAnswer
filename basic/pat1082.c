93da37497b3e0a7c80df51afddb0a4adda705b10
xiaorong61 <xiaorong61@gmail.com>
2019-02-12 14:39:42 +0800

    结构体改成数组

--- basic/pat1082.c
+++ basic/pat1082.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct Item { int ID, x, y; } Item;
-int compare(const void *left, const void *right) {
-  Item *l = (Item *)left;
-  Item *r = (Item *)right;
-  int x1 = l->x, y1 = l->y, x2 = r->x, y2 = r->y;
-  return x1 * x1 + y1 * y1 > x2 * x2 + y2 * y2 ? 1 : -1;
+enum index { ID, X, Y };
+int square(int x) { return x * x; }
+int square_sum(int x, int y) { return square(x) + square(y); }
+int compare(const void *_l, const void *_r) {
+  int *l = (int *)_l;
+  int *r = (int *)_r;
+  return square_sum(l[X], l[Y]) - square_sum(r[X], r[Y]);
 }
 int main() {
   int N;
   scanf("%d", &N);
-  Item arr[N];
+  int arr[N][Y + 1];
   for (int i = 0; i < N; i++)
-    scanf("%d %d %d", &arr[i].ID, &arr[i].x, &arr[i].y);
-  qsort(arr, N, sizeof(Item), compare);
-  printf("%04d %04d", arr[0].ID, arr[N - 1].ID);
+    scanf("%d %d %d", &arr[i][ID], &arr[i][X], &arr[i][Y]);
+  qsort(arr, N, sizeof(int[Y + 1]), compare);
+  printf("%04d %04d", arr[0][ID], arr[N - 1][ID]);
   return 0;
 }
